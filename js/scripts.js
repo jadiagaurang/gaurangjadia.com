8704c8b66cf7675f8c51d636ddfda4457a93e4b3
Gaurang Jadia <mail@gaurangjadia.com>
2022-05-12 10:08:17 -0700

    Added few more images to Joke of the Day Section!

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,46 @@
-window.addEventListener('DOMContentLoaded', event => {
-    const imgs = ["1", "2", "1", "2", "1", "2", "1", "2", "1", "2"];
-    const random = Math.floor(Math.random() * imgs.length);
-    document.getElementById("id-thought-of-the-day").src = "assets/img/art-programming/" + imgs[random] + ".jpg";
+window.addEventListener("DOMContentLoaded", event => {
+    const arrayJokeOfTheDay = ["1.jpg", "2.jpg", "3.png", "4.png", "5.png", "6.png", "7.png", "8.jpg", "9.jpg"];
+    const random = Math.floor(Math.random() * arrayJokeOfTheDay.length);
+    document.getElementById("id-thought-of-the-day").src = "assets/img/art-programming/" + arrayJokeOfTheDay[random];
 
     // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
+    const navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector("#mainNav");
         if (!navbarCollapsible) {
             return;
         }
+        
         if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+            navbarCollapsible.classList.remove("navbar-shrink");
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+            navbarCollapsible.classList.add("navbar-shrink");
         }
-
     };
 
     // Shrink the navbar 
     navbarShrink();
 
     // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+    document.addEventListener("scroll", navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
+    const mainNav = document.body.querySelector("#mainNav");
     if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
+        const objScrollSpy = new bootstrap.ScrollSpy(document.body, {
+            target: "#mainNav",
             offset: 72,
         });
-    };
+        console.log(objScrollSpy);
+    }
 
     // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const navbarToggler = document.body.querySelector(".navbar-toggler");
     const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
+        document.querySelectorAll("#navbarResponsive .nav-link")
     );
     responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        responsiveNavItem.addEventListener("click", () => {
+            if (window.getComputedStyle(navbarToggler).display !== "none") {
                 navbarToggler.click();
             }
         });
